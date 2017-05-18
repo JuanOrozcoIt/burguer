f3a95293f0c6f0819d687cc59ce572e335cd5925
JuanOrozcoIt <Juan.Orozco.it@gmail.com>
2017-05-18 15:20:14 -0700

    DB schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Double Double Animal Style');
+INSERT INTO burgers (name) VALUES ('Number 2, Protein Style');
+INSERT INTO burgers (name) VALUES ('Number 1, with Animal Fries');
